03aee386dc4033f675f9d6347d053dd04f0b506b
sweisscct <114678287+sweisscct@users.noreply.github.com>
2026-04-17 14:23:43 +0100

    JS Plans

---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+Getting and displaying names
+Placing symbols on the grid
+Changing turns
+Checking for game end
+Display end game message
+Increment Score
+Reset Game
+Start clock
+*/
+
+// name-input on click -> getAndDisplayName
+// for each cell -> on click -> placeSymbol
+// placeSymbol -> get currentPlayer, (if blank) update cell, , check for endGame, change turn
+// checkGameEnd -> check rows, cols, diagonals for win or all full 
+//      -> endGameMessage, updateScore, pauseClock
+// endGameMessage -> alert
+// updateScore -> getWinner -> get and increment score
+// restart-button -> on click -> (for each cell -> cell.innerText=""), game-time=0, startClock
+ 
